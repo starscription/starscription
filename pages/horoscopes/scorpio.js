4dce0f6347bd50df34b3f98758dc787b6c905c24
starscription <ejresendes@gmail.com>
2025-07-31 11:27:54 -0400

    Update scorpio.js

--- pages/horoscopes/scorpio.js
+++ pages/horoscopes/scorpio.js
@@ -1,12 +1,8 @@
 export default function Scorpio() {
   return (
     <div>
-      <h1>♏ Scorpio Horoscope</h1>
-      <p>
-        Someone may try to test your patience today. But don’t worry —
-        your mysterious stare will keep them guessing. Avoid major life decisions
-        before coffee and maybe double-check your texts before sending.
-      </p>
+      <h1>Scorpio</h1>
+      <p>Someone may try to test your patience today. Politely ignore them and go back to being the mysterious legend you are.</p>
     </div>
   );
 }

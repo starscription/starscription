f2ae7c25abae574764d97370d64537a8f8556b76
starscription <ejresendes@gmail.com>
2025-07-31 08:26:01 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Starscription ✨</h1>
+      <p>Funny horoscopes coming soon...</p>
+    </div>
+  );
+}
